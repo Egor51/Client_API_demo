8c9f50b19ce35cbc7c76c891114e9960863dcc17
Egor51 <kubrakov_egor87@mail.ru>
2022-07-28 17:41:43 +0300

    Creat consol v2

--- src/main/java/Api.java
+++ src/main/java/Api.java
@@ -7,8 +7,10 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.entity.ContentType;
 import org.apache.http.impl.client.CloseableHttpClient;
 import java.io.IOException;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Stream;
 
 import static org.apache.http.impl.client.HttpClientBuilder.*;
 
@@ -28,6 +30,7 @@ public class Api {
         request.setHeader(HttpHeaders.ACCEPT, ContentType.APPLICATION_JSON.getMimeType());
         CloseableHttpResponse response = httpClient.execute(request);
         List<Product> post = mapper.readValue(response.getEntity().getContent(), new TypeReference<List<Product>>() {});
+
         return post;
     }
     public static ObjectMapper mapper = new ObjectMapper();
@@ -35,10 +38,8 @@ public class Api {
     public static void main(String[] args) throws IOException {
 
         String key ="&key=YjI2YmEyYzgtNmMxNi00YzdlLWI2MGEtYzNmMTExNTQwNTMy";
-        final String urlOrders = "https://suppliers-stats.wildberries.ru/api/v1/supplier/orders?";
-        final String urlSales = "https://suppliers-stats.wildberries.ru/api/v1/supplier/sales?";
-                ;
-//
+        final String urlOrders ="https://suppliers-stats.wildberries.ru/api/v1/supplier/orders?";
+        final String urlSales ="https://suppliers-stats.wildberries.ru/api/v1/supplier/sales?";
 
         Scanner scanner = new Scanner(System.in);
 
@@ -48,7 +49,7 @@ public class Api {
                 System.out.println("----------------------------------------");
                 System.out.println("1.Заказы");
                 System.out.println("2.Выкупы");
-                System.out.println("3.Остатки");
+                System.out.println("3.Отчет за день");
                 System.out.println("4.Отчет за день");
 
         String input = scanner.nextLine();
@@ -58,29 +59,27 @@ public class Api {
                     System.out.println("Введите дату формат год-месяц-день");
                     String dateA = "dateFrom="+scanner.nextLine();
                     List<Product> post = httpClientSeller(urlOrders+dateA+key);
-                    long orderProdukt =  post.stream().count();
-                    System.out.println("Заказано " + orderProdukt + " шт");
                     post.stream().forEach(System.out::println);
+                    break;
                 case 2:
                     System.out.println("Введите дату формат год-месяц-день");
                     String dateB = "dateFrom="+scanner.nextLine();
                     List<Product> postSales = httpClientSeller(urlSales+dateB+key);
-                    long saleProdukt =  postSales.stream().count();
-                    System.out.println("Выкуплено " + saleProdukt + " шт");
                     postSales.stream().forEach(System.out::println);
+                    break;
                 case 3:
-
+                    String dateC = "dateFrom="+ LocalDate.now();;
+                    List<Product> salesDay = httpClientSeller(urlSales+dateC+key);
+                    long countProduct =  salesDay.stream().count();
+                    System.out.println("Сегодня было выкупленно: " + countProduct + " товаров");
+                    List<Product> postDay = httpClientSeller(urlOrders+dateC+key);
+                    long orderProdukt =  postDay.stream().count();
+                    System.out.println("Сегодня было заказано: " + orderProdukt + " товаров");
 
                 case 4:
 
 
-
            }
-
-
-
-
-
        }
     }
 }
